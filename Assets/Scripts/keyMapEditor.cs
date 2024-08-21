f65581c7314c8a4e728dfc4a8b904ffcfb2ac139
Kenyer <47274356+tdani01@users.noreply.github.com>
2024-08-21 17:59:14 +0200

    keymap editor ui

--- Assets/Scripts/keyMapEditor.cs
+++ Assets/Scripts/keyMapEditor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,20 +9,25 @@ public class keyMapEditor : MonoBehaviour
 {
     public GameObject keyMapPanel;
     public GameObject keyMapButtonPrefab;
-    public GameObject keyLabel; // to add
-    public Dictionary<KeyHandler.KeyAction, Button[]> keyMapButtons;
+    public GameObject keyLabelPrefab;
+    private GameObject QuitButtonPrefab;
+
+    public Dictionary<KeyHandler.KeyAction, (Button primaryKey, Button secondaryKey)> keyMapButtons;
     private KeyHandler.KeyAction currentModify;
-
-    private GameObject QuitButton;
-
+    private bool modifyprimaryKey;
 
     private bool isUIInitialized = false;
+
+    private void Start()
+    {
+        // add quit button
+    }
 
     public void ShowKeyMapEditor()
     {
         if (!isUIInitialized)
         {
-            // init editor
+            InitializeKeyMapEditorUI();
             isUIInitialized = true;
         }
         keyMapPanel.SetActive(true);
@@ -31,44 +37,78 @@ public class keyMapEditor : MonoBehaviour
 
     private void InitializeKeyMapEditorUI()
     {
-        keyMapButtons = new Dictionary<KeyHandler.KeyAction, Button[]>();
+        keyMapButtons = new Dictionary<KeyHandler.KeyAction, (Button, Button)>();
 
         foreach (KeyHandler.KeyAction action in Enum.GetValues(typeof(KeyHandler.KeyAction)))
         {
-            GameObject buttonObj = Instantiate(keyMapButtonPrefab, keyMapPanel.transform);
-
-            Button button1 = buttonObj.GetComponent<Button>();
-            Text buttonText1 = buttonObj.GetComponent<Text>();
-
-            Button button2 = buttonObj.GetComponent<Button>();
-            Text buttonText2 = buttonObj.GetComponent<Text>();
-
-            KeyCode[] keys = KeyHandler.Instance.getKeys(action);
-            
-            buttonText1.text = $"{action}: {keys[0]}";
-            buttonText2.text = $"{action}: {keys[1]}";
-
-            keyMapButtons[action] = new Button[2] { button1, button2 };
-
-            button1.onClick.AddListener(() => OnKeyMapButtonClicked(action, 0));
-            button2.onClick.AddListener(() => OnKeyMapButtonClicked(action, 1));
+            CreateKeyMappingButtonsRow(action);
         }
+
+        GameObject quitButtonObj = Instantiate(QuitButtonPrefab, keyMapPanel.transform);
+        Button quitButton = quitButtonObj.GetComponent<Button>();
+        Text quitButtonText = quitButton.GetComponentInChildren<Text>();
+        quitButtonText.text = "Vissza";
+
+        quitButton.onClick.AddListener(() => HideKeyMapEditor());
     }
 
-    private void OnKeyMapButtonClicked(KeyHandler.KeyAction keyAction, int key)
+    private void CreateKeyMappingButtonsRow(KeyHandler.KeyAction keyAction)
+    {
+        GameObject row = new GameObject(keyAction.ToString() + "_Row");
+        row.transform.SetParent(keyMapPanel.transform, false);
+
+        HorizontalLayoutGroup layoutGroup = row.AddComponent<HorizontalLayoutGroup>();
+        layoutGroup.spacing = 20;
+
+        GameObject labelObj = Instantiate(keyLabelPrefab, row.transform);
+        Text label = labelObj.GetComponent<Text>();
+        label.text = SetActionName(keyAction.ToString());
+
+        GameObject primaryButtonObj = Instantiate(keyMapButtonPrefab, row.transform);
+        Button primaryButton = primaryButtonObj.GetComponent<Button>();
+        Text primaryButtonText = primaryButton.GetComponentInChildren<Text>();
+        KeyCode[] keys = KeyHandler.Instance.getKeys(keyAction);
+        primaryButtonText.text = $"Elsõdleges: {keys[0]}";
+        //--
+        GameObject secondaryButtonObj = Instantiate(keyMapButtonPrefab, row.transform);
+        Button secondaryButton = secondaryButtonObj.GetComponent<Button>();
+        Text secondaryButtonText = secondaryButton.GetComponentInChildren<Text>();
+        secondaryButtonText.text = $"Másodlagos: {keys[1]}";
+
+        keyMapButtons[keyAction] = (primaryButton, secondaryButton);
+
+        primaryButton.onClick.AddListener(() => OnKeyMapButtonClicked(keyAction, true));
+        secondaryButton.onClick.AddListener(() => OnKeyMapButtonClicked(keyAction, false));
+    }
+
+    private string SetActionName(string actionName)
+    {
+        if (!string.IsNullOrEmpty(actionName))
+        {
+            if (actionName.Length > 15)
+                return actionName.Substring(0,15);
+            return actionName;
+        }
+        else
+            Debug.LogWarning("SetActionName got a null string from action Enum");
+        return "";
+    }
+
+    private void OnKeyMapButtonClicked(KeyHandler.KeyAction keyAction, bool isPrimaryKey)
     {
         currentModify = keyAction;
+        modifyprimaryKey = isPrimaryKey;
 
-        foreach (var buttons in keyMapButtons.Values)
+        foreach (var (primaryButton, secondaryButton) in keyMapButtons.Values)
         {
-            buttons[0].interactable = false;
-            buttons[1].interactable = false;
+            primaryButton.interactable = false;
+            secondaryButton.interactable = false;
         }
 
-        StartCoroutine(WaitForKeyInput(key));
+        StartCoroutine(WaitForKeyInput());
     }
 
-    private IEnumerator WaitForKeyInput(int key)
+    private IEnumerator WaitForKeyInput()
     {
         while (!Input.anyKeyDown)
             yield return null;
@@ -82,27 +122,38 @@ public class keyMapEditor : MonoBehaviour
                 break;
             }
         }
-
+        
         if (newKey != KeyCode.None)
         {
-            switch (key)
+            if (modifyprimaryKey)
             {
-                case 0:
-                    KeyHandler.Instance.ModifyMap(currentModify, newKey1: newKey);
-                    keyMapButtons[currentModify][0].GetComponentInChildren<Text>().text = $"{currentModify}: {newKey}";
-                    break;
-
-                case 1:
-                    KeyHandler.Instance.ModifyMap(currentModify, newKey2: newKey);
-                    keyMapButtons[currentModify][1].GetComponentInChildren<Text>().text = $"{currentModify}: {newKey}";
-                    break;
-            }            
+                KeyHandler.Instance.ModifyMap(currentModify, newKey1: newKey);
+                keyMapButtons[currentModify].primaryKey.GetComponentInChildren<Text>().text = $"Elsõdleges: {newKey}";
+            }
+            else
+            {
+                KeyHandler.Instance.ModifyMap(currentModify, newKey2: newKey);
+                keyMapButtons[currentModify].primaryKey.GetComponentInChildren<Text>().text = $"Másodlagos: {newKey}";
+            }
         }
 
-        foreach (var buttons in keyMapButtons.Values)
+        foreach (var (primaryButton, secondaryButton) in keyMapButtons.Values)
         {
-            buttons[0].interactable = true;
-            buttons[1].interactable = true;
+            primaryButton.interactable = true;
+            secondaryButton.interactable = true;
+        }
+    }
+
+    private IEnumerator CheckForEscapeKey()
+    {
+        while (true)
+        {
+            if (KeyHandler.Instance.GetKeyDown(KeyHandler.KeyAction.i_Back))
+            {
+                HideKeyMapEditor();
+                break;
+            }
+            yield return null;
         }
     }
 }
