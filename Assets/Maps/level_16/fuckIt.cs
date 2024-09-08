bf063e679ce7eb579b767d529921266355646ad6
Kenyer <47274356+tdani01@users.noreply.github.com>
2024-09-08 10:37:04 +0200

    .

---
+++ Assets/Maps/level_16/fuckIt.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fuckIt : MonoBehaviour
+{
+    
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Debug.Log("Width: "+Screen.width+"\nHeight: "+Screen.height);
+        Dictionary<string>
+    }
+}
+
